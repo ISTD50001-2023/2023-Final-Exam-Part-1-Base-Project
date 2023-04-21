46aeae926cc03a60840516e92ddc7d4787bd2aa2
otaku.prof <norman_lee@sutd.edu.sg>
2023-04-21 09:56:53 +0800

    initial commit

---
+++ app/src/main/java/com/example/a50001finalexampart12023baseproject/TestCases.java
@@ -0,0 +1,18 @@
+package com.example.a50001finalexampart12023baseproject;
+
+public class TestCases {
+
+    public static void main(String[] args) {
+
+        /*** ATTENTION
+         * To display the output of the main() function of any Java class in an
+         * Android App project, click the green arrow and select Run With Coverage
+         * (You may have to click it more than once).
+         */
+
+        System.out.println("--------------------");
+        System.out.println("Dummy Test Case File");
+        System.out.println("--------------------");
+
+    }
+}
